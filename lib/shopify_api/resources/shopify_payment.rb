cf10b8e0220dab85bdcb027fa8bb94782c042eab
sonika <sonika@bajratechnologies.com>
2023-12-20 17:49:40 +0545

    fix: update the shopify payment prefix and path for api call

--- lib/shopify_api/resources/shopify_payment.rb
+++ lib/shopify_api/resources/shopify_payment.rb
@@ -1,9 +1,13 @@
 module ShopifyAPI
   class ShopifyPayout < Base
-    self.prefix = '/admin/shopify_payments/payouts/'
+    self.prefix = 'shopify_payments/payouts/'
 
     def self.find_search(date_min=Time.zone.now.beginning_of_month.to_date.to_s, date_max=Time.zone.now.end_of_month.to_date.to_s)
-      self.find(:all, from: "/admin/shopify_payments/payouts.json?date_min=#{date_min}&date_max=#{date_max}")
+      self.find(:all, 
+                from: api_version.construct_api_path(
+                    "shopify_payments/payouts.json?date_min=#{date_min}&date_max=#{date_max}"
+                  )
+                )
     end
   end
 end
